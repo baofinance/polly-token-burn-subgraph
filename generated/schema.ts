e4b2b1b3d02fa40a494cf0be7fc9862f172d2d02
Ben Clabby <benclabby@gmail.com>
2021-08-31 01:37:09 -0400

    Add supply tracker and point subgraph at test token

--- generated/schema.ts
+++ generated/schema.ts
@@ -60,3 +60,43 @@ export class Burn extends Entity {
     this.set("eventCount", Value.fromBigInt(value));
   }
 }
+
+export class TokenStats extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save TokenStats entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save TokenStats entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("TokenStats", id.toString(), this);
+  }
+
+  static load(id: string): TokenStats | null {
+    return store.get("TokenStats", id) as TokenStats | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get supply(): BigInt {
+    let value = this.get("supply");
+    return value.toBigInt();
+  }
+
+  set supply(value: BigInt) {
+    this.set("supply", Value.fromBigInt(value));
+  }
+}
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,28 +1,46 @@
 import { Address, BigInt, log } from '@graphprotocol/graph-ts'
 import { Transfer } from '../generated/PollyToken/PollyToken'
-import { Burn } from '../generated/schema'
+import { Burn, TokenStats } from '../generated/schema'
 
 const genesisAddress = Address.fromString(
   '0x0000000000000000000000000000000000000000'
 )
 
 export function handleTransfer(event: Transfer): void {
-  if (event.params.to == genesisAddress) {
-    log.info('{} burned {} Polly', [
-      event.params.from.toHex(),
-      event.params.value.toString()
-    ])
-    let entity = Burn.load('0')
+  const value = event.params.value
+  const from = event.params.from
+  const to = event.params.to
 
-    if (entity == null) {
-      entity = new Burn('0')
-      entity.burnedTokens = BigInt.fromI32(0)
-      entity.eventCount = BigInt.fromI32(0)
+  let tokenStats = TokenStats.load('0')
+  if (tokenStats == null) {
+    tokenStats = new TokenStats('0')
+    tokenStats.supply = BigInt.fromI32(0)
+    tokenStats.save()
+  }
+
+  if (from == genesisAddress) {
+    tokenStats.supply = tokenStats.supply.plus(value)
+    tokenStats.save()
+  }
+
+  if (to == genesisAddress) {
+    log.info('{} burned {} Polly', [
+      from.toHex(),
+      value.toString()
+    ])
+    tokenStats.supply = tokenStats.supply.minus(value)
+    let burnStats = Burn.load('0')
+
+    if (burnStats == null) {
+      burnStats = new Burn('0')
+      burnStats.burnedTokens = BigInt.fromI32(0)
+      burnStats.eventCount = BigInt.fromI32(0)
     }
 
-    entity.burnedTokens = entity.burnedTokens.plus(event.params.value)
-    entity.eventCount = entity.eventCount.plus(BigInt.fromI32(1))
+    burnStats.burnedTokens = burnStats.burnedTokens.plus(value)
+    burnStats.eventCount = burnStats.eventCount.plus(BigInt.fromI32(1))
 
-    entity.save()
+    burnStats.save()
+    tokenStats.save()
   }
 }
