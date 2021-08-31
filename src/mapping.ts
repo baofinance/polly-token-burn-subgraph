c6e8170d770e0c34f4deeba6747526ad42257b9b
Ben Clabby <benclabby@gmail.com>
2021-08-31 00:08:01 -0400

    Fix entity not propogating initially

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,23 +1,28 @@
-import { BigInt } from '@graphprotocol/graph-ts'
+import { Address, BigInt, log } from '@graphprotocol/graph-ts'
 import { Transfer } from '../generated/PollyToken/PollyToken'
 import { Burn } from '../generated/schema'
 
-const genesisAddress = '0x0000000000000000000000000000000000000000'
+const genesisAddress = Address.fromString(
+  '0x0000000000000000000000000000000000000000'
+)
 
 export function handleTransfer(event: Transfer): void {
-  if (event.transaction.from.toHex() !== genesisAddress)
-    return;
+  if (event.params.to == genesisAddress) {
+    log.info('{} burned {} Polly', [
+      event.params.from.toHex(),
+      event.params.value.toString()
+    ])
+    let entity = Burn.load('0')
 
-  let entity = Burn.load('0')
+    if (entity == null) {
+      entity = new Burn('0')
+      entity.burnedTokens = BigInt.fromI32(0)
+      entity.eventCount = BigInt.fromI32(0)
+    }
 
-  if (entity == null) {
-    entity = new Burn('0')
-    entity.burnedTokens = BigInt.fromI32(0)
-    entity.eventCount = BigInt.fromI32(0)
+    entity.burnedTokens = entity.burnedTokens.plus(event.params.value)
+    entity.eventCount = entity.eventCount.plus(BigInt.fromI32(1))
+
+    entity.save()
   }
-
-  entity.burnedTokens = entity.burnedTokens.plus(event.params.value)
-  entity.eventCount = entity.eventCount.plus(BigInt.fromI32(1))
-
-  entity.save()
 }
